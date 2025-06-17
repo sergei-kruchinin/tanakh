21ad483a530ea90d2a31134afbca0b33850719e8
kruchinin.s <kruchinin.sergei.al@gmail.com>
2025-06-17 21:56:00 +0300

    Копия https://github.com/onlinebible/onlinebible.github.io

---
+++ russian-interlinear-html/header.js
@@ -0,0 +1,12 @@
+﻿function Header () {
+  document.write("<div class='top-space'></div>");
+  document.write("<TABLE class='top-menu'><TR>");
+  document.write("<TD width='1' title='Предыдущая глава'>");
+	if (Back == '') { document.write("<BUTTON class='menu-button' disabled>&#8249;</BUTTON>"); }
+	else { document.write("<BUTTON class='menu-button' onClick=\"window.location='"+Back+"'\">&#8249;</BUTTON>"); }
+  document.write("</TD><TD><DIV id='header'><a href='"+Up+"' title='Оглавление'>"+Name+"</a></DIV></TD>");
+  document.write("<TD width='1' title='Следующая глава'>");
+	if (Next == '') { document.write("<BUTTON class='menu-button' disabled>&#8250;</BUTTON>"); }
+	else { document.write("<BUTTON class='menu-button' onClick=\"window.location='"+Next+"'\">&#8250;</BUTTON>"); }
+  document.write("</TD></TR></TABLE>");
+}
